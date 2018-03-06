9d49dc84bd487e75a5c0559c0718ae03b90cf0f2
Marçal Isern <marsal.isern@qubiq.es>
2018-03-06 17:23:54 +0100

    [ADD] project_crm_lead_link

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Available addons
 addon | version | summary
 --- | --- | ---
 [project_category](project_category/) | 10.0.1.0.0 | Tipos de proyecto
+[project_crm_lead_link](project_crm_lead_link/) | 10.0.1.0.0 | Relación inversa entre proyecto y Oportunidad
 [project_sale_order_link](project_sale_order_link/) | 10.0.1.0.0 | Relación inversa entre proyecto y pedido de venta
 [qu_project_code](qu_project_code/) | 10.0.1.0.0 | Añade una secuencia a los proyectos
 
---
+++ project_crm_lead_link/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 QubiQ <marsal.isern@qubiq.es>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Crm Link",
+    "version": "10.0.1.0.0",
+    "category": "Project Management",
+    "author": "QubiQ, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.qubiq.es",
+    "license": "AGPL-3",
+    "contributors": [
+        "Marçal Isern <marsal.isern@qubiq.es>",
+    ],
+    "depends": [
+        "project",
+        "crm_timesheet",
+
+    ],
+    "data": [
+
+        "views/project_view.xml",
+    ],
+    'installable': True,
+
+}
