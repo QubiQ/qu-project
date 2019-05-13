fbe9f0d5da11764a83b8de0d7186d30c7a14451e
valentin vinagre urteaga <valentin.vinagre@qubiq.es>
2019-05-13 17:24:19 +0200

    [ADD]project_contracts se ha añadido la funcionalidad de indicar si la entrada de la cuenta analítica cuenta como imputable o no.

---
+++ project_contracts/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright (c) 2018 QubiQ (http://www.qubiq.es)
+
+from . import product_category
+from . import project
+from . import project_contract
+from . import account_analytic_line
+from . import res_users
+from . import project_task
+from . import account_invoice
+from . import res_partner
