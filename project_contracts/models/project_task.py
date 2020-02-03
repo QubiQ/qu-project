bb2fadeff38cfcaa83280f297e93f62c64cede83
xavierjimenez <xavier.jimenez@qubiq.es>
2020-02-03 14:00:30 +0100

    [ADD] Changes for billable/non-billable hours

--- project_contracts/models/project_task.py
+++ project_contracts/models/project_task.py
@@ -3,7 +3,8 @@
 # Copyright (c) 2018 QubiQ (http://www.qubiq.es)
 
 
-from odoo import models, fields, _
+from odoo import models, fields, api, _
+import logging
 
 
 class ProjectTask(models.Model):
@@ -13,3 +14,51 @@ class ProjectTask(models.Model):
         string=_('Beyond Scope'),
         default=False
     )
+
+    billable_hours = fields.Float(compute='_compute_project_billable_worked_hours',
+        readonly=True, string='Billable hours')
+
+    no_billable_hours = fields.Float(compute='_compute_project_billable_worked_hours', 
+        readonly=True, string='No billable hours')
+
+    billable_child_hours = fields.Float(compute='_compute_project_billable_worked_hours',
+        readonly=True, string='SubTask Billable Hours')
+
+    no_billable_child_hours = fields.Float(compute='_compute_project_billable_worked_hours', 
+        readonly=True, string='SubTask No Billable Hours')
+
+    remaining_billable_hours = fields.Float(compute='_compute_project_billable_worked_hours', 
+        readonly=True, string='Remaining Billable Hours')
+
+    total_billable_hours_spent = fields.Float(compute='_compute_project_billable_worked_hours', 
+        readonly=True, string='Total Billable Hours')
+
+    type_project = fields.Selection(related='project_id.type_project')
+    
+
+
+
+    @api.multi
+    def _compute_project_billable_worked_hours(self):
+        for sel in self:    
+            sel.billable_hours = 0
+            sel.no_billable_hours = 0  
+            sel.billable_child_hours = 0
+            sel.no_billable_child_hours = 0       
+            for line in sel.timesheet_ids:
+                if line.billable:
+                    sel.billable_hours += line.unit_amount
+                else:
+                    sel.no_billable_hours += line.unit_amount
+            if len(sel.child_ids) != 0:
+                for child_task in sel.child_ids:
+                    for child_line in child_task.timesheet_ids:
+                        if child_line.billable:
+                            sel.billable_child_hours += child_line.unit_amount
+                        else:
+                            sel.no_billable_child_hours += child_line.unit_amount
+
+            sel.total_billable_hours_spent = sel.billable_child_hours + sel.billable_hours
+            sel.remaining_billable_hours = sel.planned_hours - sel.total_billable_hours_spent
+            
+
