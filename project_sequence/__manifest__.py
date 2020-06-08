918cbb72fcb0b5c54f3d14bbccdfc4e29e8c4a7b
Jesus Ramoneda <chusamo@gmail.com>
2020-06-08 15:48:37 +0200

    [ADD] project_sequence: module for add sequences in project names

---
+++ project_sequence/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Jesus Ramoneda <jesus.ramoneda@qubiq.es>
+# License AGPL-3.0 or later (https: //www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Sequence",
+    "summary": "Project Sequence",
+    "version": "13.0.1.0.0",
+    "category": "Pro",
+    "website": "https://www.qubiq.es",
+    "author": "QubiQ, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'project'
+    ],
+    "data": [
+        "data/ir_sequence.xml",
+    ],
+}
