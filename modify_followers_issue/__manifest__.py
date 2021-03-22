6978b4a3ea4847bfbed7043447197f90cefb7c5d
Jesus Ramoneda <jesus.ramoneda@qubiq.es>
2021-03-22 16:06:07 +0100

    [13.0][MIG] modify_followers_issue: Migration to 13.0

--- modify_followers_issue/__manifest__.py
+++ modify_followers_issue/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Modify Followers Issue",
     "summary": "Modify list of followers from a helpdesk ticket",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Helpdesk",
     "website": "https://www.qubiq.es",
     "author": "QubiQ, Odoo Community Association (OCA)",
